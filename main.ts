02c60eb5fd26914b0981dff237f08e586f19ad7a
phattaraphumj <phattaraphumj@gmail.com>
2026-01-31 20:41:16 +0700

    Function completed + Passed all tests

--- main.ts
+++ main.ts
@@ -4,5 +4,20 @@ export function romanToInt(s: string): number {
         'C': 100, 'D': 500, 'M': 1000
     };
     let num = 0;
+    for (let i = s.length - 1; i >= 0; i--) {
+        const char = s[i] as string; 
+        const currentVal = romanValues[char] ?? 0;
+
+        if (i == s.length - 1){
+            num += currentVal;
+            continue;
+        }
+
+        if (currentVal < (romanValues[s[i + 1] as string] ?? 0)) {
+            num -= currentVal;
+        } else {
+            num += currentVal;
+        }
+    }
     return num;
 }
